92492752f924734a62d3733753cd65211e9862a8
moresudesh <sudesh.more@adfactorspr.local>
2023-02-19 15:55:46 +0530

    on click body color change pure javascript

---
+++ custom.js
@@ -0,0 +1,16 @@
+
+
+var index = 0; 
+
+function changeColors(){
+
+var colors = ['red', 'green', 'blue', 'pink', 'purple'];
+
+document.getElementsByTagName('body')[0].style.background = colors[index++];
+
+if(index > colors.length - 1){
+ index = 0;}
+
+ 
+}
+
